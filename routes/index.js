220e0a226165631c830f7d2ea39f03c569f13b56
Vincent Montalieu <vincent.montalieu@outlook.com>
2015-08-24 17:34:30 +0200

    MAJ du serveur

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('home');
+router.get('/', function (req, res, next) {
+    res.send('home');
 });
 
 module.exports = router;
--- routes/recog.js
+++ routes/recog.js
@@ -3,18 +3,41 @@
  * @brief Routes for recog.
  */
 
-// Requirements
+/**
+ * Requirements
+ */
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var multer = require('multer');
+var uploads = multer({
+    dest: 'uploads/',
+    rename: function (fieldname, filename) {
+    	console.log("Rename...");
+        return filename + Date.now();
+    },
+    onFileUploadStart: function () {
+        console.log("Upload is starting...");
+    },
+    onFileUploadComplete: function () {
+        console.log("File uploaded");
+    }
+});
 
 /**
- * Test de communication
+ * Lancement du moteur de recherche de l'image
  */
-router.post('/upload', upload.single('image'), function (req, res, next) {
-	console.log("Front-end is calling !");
-    res.json({status: 'success', data: 'Fichier chargé.\nOrgane sélectionné : ' + req.body.organ});
+var launchRecog = function (path, organ) {
+	setTimeout()
+};
+
+/**
+ * Upload d'une image
+ */
+router.post('/upload', uploads.single('image'), function (req, res, next) {
+    console.log("Front-end is calling");
+    console.log(req.body.organ);
+    res.json({status: 'success', data: 'Terminé'});
+    launchRecog("lol", "mdr");x
 });
 
 module.exports = router;
